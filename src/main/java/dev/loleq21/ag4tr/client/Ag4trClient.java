ef8467f3e10f28f715b1730f2751b30dc1157892
loleq2105 <emkarol21gmail.com>
2020-10-15 18:18:45 +0200

    add the composite shield plus stuff

--- src/main/java/dev/loleq21/ag4tr/client/Ag4trClient.java
+++ src/main/java/dev/loleq21/ag4tr/client/Ag4trClient.java
@@ -5,10 +5,8 @@ import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
 import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
-import net.fabricmc.fabric.api.client.rendering.v1.ColorProviderRegistry;
 import net.minecraft.client.options.KeyBinding;
 import net.minecraft.client.util.InputUtil;
-import net.minecraft.item.DyeableItem;
 import org.lwjgl.glfw.GLFW;
 
 @Environment(EnvType.CLIENT)
@@ -18,15 +16,17 @@ import org.lwjgl.glfw.GLFW;
             InputUtil.Type.KEYSYM,
             GLFW.GLFW_KEY_N,
             "category.ag4tr.title");
-    public static final KeyBinding EXOLEGS_JUMP_BOOST_KEY_BIND = new KeyBinding("key.ag4tr.toggle_jb",
+    /*public static final KeyBinding EXOLEGS_JUMP_BOOST_KEY_BIND = new KeyBinding("key.ag4tr.toggle_jb",
             InputUtil.Type.KEYSYM,
             GLFW.GLFW_KEY_B,
             "category.ag4tr.title");
+
+     */
 
     @Override
     public void onInitializeClient() {
         Ag4trStackToolTipHandler.setup();
         KeyBindingHelper.registerKeyBinding(NV_KEY_BIND);
-        KeyBindingHelper.registerKeyBinding(EXOLEGS_JUMP_BOOST_KEY_BIND);
+        //KeyBindingHelper.registerKeyBinding(EXOLEGS_JUMP_BOOST_KEY_BIND);
     }
 }
