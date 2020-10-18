4e263e5ef05a172f6aeb369d13bb8dd4559048c8
loleq2105 <emkarol21gmail.com>
2020-10-18 19:12:43 +0200

    improve underwater RHM suit mechanics + corrections

--- src/main/java/dev/loleq21/ag4tr/RHMChestPiece.java
+++ src/main/java/dev/loleq21/ag4tr/RHMChestPiece.java
@@ -2,15 +2,19 @@ package dev.loleq21.ag4tr;
 
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
+import net.minecraft.client.item.TooltipContext;
 import net.minecraft.entity.EquipmentSlot;
 import net.minecraft.entity.effect.StatusEffectInstance;
 import net.minecraft.entity.effect.StatusEffects;
 import net.minecraft.entity.player.PlayerEntity;
-import net.minecraft.item.ArmorItem;
-import net.minecraft.item.ArmorMaterial;
-import net.minecraft.item.ItemGroup;
-import net.minecraft.item.ItemStack;
+import net.minecraft.item.*;
+import net.minecraft.nbt.CompoundTag;
+import net.minecraft.text.LiteralText;
+import net.minecraft.text.Text;
+import net.minecraft.util.Formatting;
 import net.minecraft.util.collection.DefaultedList;
+import net.minecraft.world.World;
+import org.jetbrains.annotations.Nullable;
 import reborncore.api.items.ArmorTickable;
 import reborncore.common.powerSystem.PowerSystem;
 import reborncore.common.util.ItemDurabilityExtensions;
@@ -19,6 +23,8 @@ import team.reborn.energy.Energy;
 import team.reborn.energy.EnergyHolder;
 import team.reborn.energy.EnergyTier;
 import techreborn.utils.InitUtils;
+
+import java.util.List;
 
 public class RHMChestPiece extends ArmorItem implements ArmorTickable, EnergyHolder, ItemDurabilityExtensions {
 
@@ -46,14 +52,47 @@ public class RHMChestPiece extends ArmorItem implements ArmorTickable, EnergyHol
                 playerEntity.extinguish();
             }
         }
-            if (playerEntity.isSubmergedInWater()) {
-                   if (canWaterFight && this.slot == EquipmentSlot.CHEST) {
-                       if (Energy.of(itemStack).use(8)) {
-                           playerEntity.addStatusEffect(new StatusEffectInstance(StatusEffects.WATER_BREATHING, 5, 1, false, false, false));
-                       }
-                   }
+        if (this.slot == EquipmentSlot.CHEST) {
+            if (!playerEntity.isSubmergedInWater()) {
+                if (!(getStoredAir(itemStack) == 256) && Energy.of(itemStack).use(2)) {
+                    setStoredAir(itemStack, getStoredAir(itemStack) + 1);
+                }
+            } else {
+                if (canWaterFight) {
+                    if (!(getStoredAir(itemStack) == 0)) {
+                        playerEntity.addStatusEffect(new StatusEffectInstance(StatusEffects.WATER_BREATHING, 5, 1, false, false, false));
+                        setStoredAir(itemStack, getStoredAir(itemStack) - 1);
+                    }
                 }
             }
+        }
+    }
+
+    public int getStoredAir(ItemStack stack) {
+        validateAirNBTTag(stack);
+        return stack.getTag().getInt("air");
+    }
+
+    public void setStoredAir(ItemStack stack, int amount) {
+        validateAirNBTTag(stack);
+        stack.getTag().putInt("air", amount);
+
+    }
+
+    private void validateAirNBTTag(ItemStack stack) {
+        if (!stack.getTag().contains("air", 3)){
+            stack.getTag().putInt("air", 0);
+        }
+    }
+
+    public int getStoredAir4ToolTip(ItemStack stack) {
+        if (stack.hasTag()) {
+            return stack.getTag().getInt("air");
+        }
+        else {
+            return 0;
+        }
+    }
 
     @Override
     public double getDurability(ItemStack stack) {
@@ -83,6 +122,18 @@ public class RHMChestPiece extends ArmorItem implements ArmorTickable, EnergyHol
     @Override
     public EnergyTier getTier() {
         return EnergyTier.MEDIUM;
+    }
+
+    @Environment(EnvType.CLIENT)
+    @Override
+    public void appendTooltip(ItemStack stack, @Nullable World worldIn, List<Text> tooltip, TooltipContext flagIn) {
+        LiteralText line1 = new LiteralText(String.valueOf(getStoredAir4ToolTip(stack)));
+        line1.append("/");
+        line1.append("256");
+        line1.append(" ");
+        line1.append("Air");
+        line1.formatted(Formatting.GRAY);
+        tooltip.add(1, line1);
     }
 
     @Environment(EnvType.CLIENT)
