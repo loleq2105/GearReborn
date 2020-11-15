9d243dfe10eb343b335750e75e481de60820757b
loleq21 <emkarol21@gmail.com>
2020-11-15 21:17:37 +0100

    fixed triggering visual quirks

--- src/main/java/dev/loleq21/ag4tr/Ag4trContent.java
+++ src/main/java/dev/loleq21/ag4tr/Ag4trContent.java
@@ -20,15 +20,17 @@ public class Ag4trContent {
 
     private static final Map<Identifier, Item> AG4TRITEMS = new LinkedHashMap<>();
 
-    public static final Item RUBBER_BOOTS = addItem("rubber_boots", new RubberArmorPiece(RUBBER_ARMOR_MATERIAL, EquipmentSlot.FEET));
+    public static final Item RUBBER_HELMET = addItem("rubber_helmet", new RubberArmorPiece(RUBBER_ARMOR_MATERIAL, EquipmentSlot.HEAD));
     public static final Item RUBBER_CHESTPLATE = addItem("rubber_chestplate", new RubberArmorPiece(RUBBER_ARMOR_MATERIAL, EquipmentSlot.CHEST));
     public static final Item RUBBER_LEGGINGS = addItem("rubber_leggings", new RubberArmorPiece(RUBBER_ARMOR_MATERIAL, EquipmentSlot.LEGS));
-    public static final Item RUBBER_HELMET = addItem("rubber_helmet", new RubberArmorPiece(RUBBER_ARMOR_MATERIAL, EquipmentSlot.HEAD));
+    public static final Item RUBBER_BOOTS = addItem("rubber_boots", new RubberArmorPiece(RUBBER_ARMOR_MATERIAL, EquipmentSlot.FEET));
+
+    public static final Item RHM_HELMET = addItem("rhm_helmet", new RHMPeripheralsLol(HAZMAT_ARMOR_MATERIAL, EquipmentSlot.HEAD));
     public static final Item RHM_CHESTPLATE = addItem("rhm_chestplate", new RHMChestPiece(HAZMAT_ARMOR_MATERIAL, EquipmentSlot.CHEST));
     public static final Item RHM_LEGGINGS = addItem("rhm_leggings", new RHMPeripheralsLol(HAZMAT_ARMOR_MATERIAL, EquipmentSlot.LEGS));
-    public static final Item RHM_HELMET = addItem("rhm_helmet", new RHMPeripheralsLol(HAZMAT_ARMOR_MATERIAL, EquipmentSlot.HEAD));
-    public static final Item TASER = addItem("taser", new TaserItem());
+
     public static final Item NV_GOGGLES = addItem("nv_goggles", new NightvisionGoggles(MISCGEAR_ARMOR_MATERIAL, EquipmentSlot.HEAD));
+    public static final Item TASER = addItem("taser", new TaserItem());
 
     private static <I extends Item> I addItem(String name, I item) {
         AG4TRITEMS.put(new Identifier(Ag4tr.MOD_ID, name), item);
