09cd393a50e27d679039a1ed1abfbeb1d4dd8cca
loleq21 <emkarol21@gmail.com>
2021-07-10 15:12:12 +0200

    add particles to rubber boots and buff their armor rating

--- src/main/java/dev/loleq21/gearreborn/GRArmorMaterials.java
+++ src/main/java/dev/loleq21/gearreborn/GRArmorMaterials.java
@@ -13,7 +13,7 @@ import java.util.function.Supplier;
 
 public enum GRArmorMaterials implements ArmorMaterial {
 
-    RUBBER(5, new int[]{1, 3, 3, 2}, 10, SoundEvents.ITEM_ARMOR_EQUIP_GENERIC, 0.0F, 0F, () -> {
+    RUBBER(5, new int[]{2, 3, 3, 2}, 10, SoundEvents.ITEM_ARMOR_EQUIP_GENERIC, 0.0F, 0F, () -> {
         return Ingredient.ofItems(TRContent.Parts.RUBBER.asItem());
     }),
     MISCGEAR(15, new int[]{1, 3, 4, 1}, 9, SoundEvents.ITEM_ARMOR_EQUIP_GENERIC, 0.0F, 0.0F, () -> {
