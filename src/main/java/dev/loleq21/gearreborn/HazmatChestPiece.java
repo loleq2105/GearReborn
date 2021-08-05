1ad4768ed91e947290d5f8845112734cbc279284
loleq21 <emkarol21@gmail.com>
2021-08-05 19:52:49 +0200

    functional hazmat suit dyeability!

--- src/main/java/dev/loleq21/gearreborn/HazmatChestPiece.java
+++ src/main/java/dev/loleq21/gearreborn/HazmatChestPiece.java
@@ -9,10 +9,8 @@ import net.minecraft.entity.effect.StatusEffectInstance;
 import net.minecraft.entity.effect.StatusEffects;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.fluid.Fluid;
-import net.minecraft.item.ArmorItem;
-import net.minecraft.item.ArmorMaterial;
-import net.minecraft.item.ItemGroup;
-import net.minecraft.item.ItemStack;
+import net.minecraft.item.*;
+import net.minecraft.nbt.NbtCompound;
 import net.minecraft.text.LiteralText;
 import net.minecraft.text.Text;
 import net.minecraft.text.TranslatableText;
@@ -38,7 +36,7 @@ import java.util.List;
 import static dev.loleq21.gearreborn.HazmatSuitUtils.playerIsWearingChestAndHelm;
 import static dev.loleq21.gearreborn.HazmatSuitUtils.playerIsWearingFullHazmat;
 
-public class HazmatChestPiece extends ArmorItem implements ArmorBlockEntityTicker, EnergyHolder, ItemDurabilityExtensions, ArmorRemoveHandler {
+public class HazmatChestPiece extends DyeableArmorItem implements ArmorBlockEntityTicker, EnergyHolder, ItemDurabilityExtensions, ArmorRemoveHandler {
 
     public HazmatChestPiece(ArmorMaterial material, EquipmentSlot slot) {
         super(material, slot, new Settings().group(GearReborn.ITEMGROUP).maxCount(1).fireproof().maxDamage(-1));
@@ -236,6 +234,12 @@ public class HazmatChestPiece extends ArmorItem implements ArmorBlockEntityTicke
     @Override
     public void onRemoved(PlayerEntity playerEntity) {
         removeEffects(playerEntity);
+    }
+
+    @Override
+    public int getColor(ItemStack stack) {
+        NbtCompound nbtCompound = stack.getSubNbt("display");
+        return nbtCompound != null && nbtCompound.contains("color", 99) ? nbtCompound.getInt("color") : 16383998 ;
     }
 }
 
--- src/main/java/dev/loleq21/gearreborn/HazmatPeripherals.java
+++ src/main/java/dev/loleq21/gearreborn/HazmatPeripherals.java
@@ -3,11 +3,20 @@ package dev.loleq21.gearreborn;
 import net.minecraft.entity.EquipmentSlot;
 import net.minecraft.item.ArmorItem;
 import net.minecraft.item.ArmorMaterial;
+import net.minecraft.item.DyeableArmorItem;
+import net.minecraft.item.ItemStack;
+import net.minecraft.nbt.NbtCompound;
 
-public class HazmatPeripherals extends ArmorItem {
+public class HazmatPeripherals extends DyeableArmorItem {
 
     public HazmatPeripherals(ArmorMaterial material, EquipmentSlot slot) {
         super(material, slot, new Settings().group(GearReborn.ITEMGROUP).maxCount(1).fireproof());
+    }
+
+    @Override
+    public int getColor(ItemStack stack) {
+        NbtCompound nbtCompound = stack.getSubNbt("display");
+        return nbtCompound != null && nbtCompound.contains("color", 99) ? nbtCompound.getInt("color") : 16383998 ;
     }
 
 }
