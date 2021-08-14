04aee72942bc8a5025c73ed0b94d69a342a08928
loleq21 <emkarol21@gmail.com>
2021-08-14 22:25:31 +0200

    make nvg toggling utilize packets, fixing issue #6

--- src/main/java/dev/loleq21/gearreborn/GearReborn.java
+++ src/main/java/dev/loleq21/gearreborn/GearReborn.java
@@ -4,7 +4,9 @@ import me.sargunvohra.mcmods.autoconfig1u.AutoConfig;
 import me.sargunvohra.mcmods.autoconfig1u.serializer.JanksonConfigSerializer;
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.client.itemgroup.FabricItemGroupBuilder;
+import net.fabricmc.fabric.api.networking.v1.ServerPlayNetworking;
 import net.minecraft.entity.EntityType;
+import net.minecraft.entity.EquipmentSlot;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.ItemGroup;
 import net.minecraft.item.ItemStack;
@@ -20,8 +22,10 @@ public class GearReborn implements ModInitializer {
     public static final String MOD_ID = "gearreborn";
     public static final ItemGroup ITEMGROUP = FabricItemGroupBuilder.create(new Identifier(MOD_ID, "items")).icon(() -> new ItemStack(GRContent.HAZMAT_HELMET)).build();
 
-    public static final EntityType[] bossMobsArray = new EntityType[] {EntityType.ENDER_DRAGON, EntityType.WITHER};
+    public static final EntityType[] bossMobsArray = new EntityType[]{EntityType.ENDER_DRAGON, EntityType.WITHER};
     public static final Set<EntityType> bossMobs = new HashSet<EntityType>(Arrays.asList(bossMobsArray));
+
+    public static final Identifier gogglesTogglePackedIdentifier = new Identifier(MOD_ID, "nvg_toggle");
 
     @Override
     public void onInitialize() {
@@ -29,13 +33,17 @@ public class GearReborn implements ModInitializer {
         GRContent.registerGearRebornContent();
 
         CableElectrocutionEvent.EVENT.register((livingEntity, cableType, blockPos, world, cableBlockEntity) -> {
-            if(livingEntity instanceof PlayerEntity) {
+            if (livingEntity instanceof PlayerEntity) {
                 PlayerEntity player = (PlayerEntity) livingEntity;
                 if (HazmatSuitUtils.playerIsWearingFullHazmat(player)) {
                     return false;
                 }
             }
             return true;
+        });
+
+        ServerPlayNetworking.registerGlobalReceiver(gogglesTogglePackedIdentifier, (client, handler, responseSender, buf, player) -> {
+                handler.getEquippedStack(EquipmentSlot.HEAD).getOrCreateNbt().putBoolean("switchPressed", buf.readBoolean());
         });
     }
 }
