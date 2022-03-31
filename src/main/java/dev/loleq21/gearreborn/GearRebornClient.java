66eebfa05950b7d51f72290db03590cda975bc1d
loleq2105 <emkarol21@gmail.com>
2022-03-31 18:39:55 +0200

    ultimately refine nvg toggling
    i am disappointed in myself for not doing this ages ago

--- src/main/java/dev/loleq21/gearreborn/GearRebornClient.java
+++ src/main/java/dev/loleq21/gearreborn/GearRebornClient.java
@@ -4,7 +4,10 @@ import me.sargunvohra.mcmods.autoconfig1u.AutoConfig;
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
+import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
 import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
+import net.fabricmc.fabric.api.client.networking.v1.ClientPlayNetworking;
+import net.fabricmc.fabric.api.networking.v1.PacketByteBufs;
 import net.minecraft.client.item.UnclampedModelPredicateProvider;
 import net.minecraft.client.option.KeyBinding;
 import net.minecraft.client.util.InputUtil;
@@ -12,8 +15,10 @@ import net.minecraft.client.world.ClientWorld;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
+import net.minecraft.text.LiteralText;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
+import net.minecraft.world.tick.Tick;
 import org.jetbrains.annotations.Nullable;
 import org.lwjgl.glfw.GLFW;
 import reborncore.common.util.ItemUtils;
@@ -23,6 +28,7 @@ import team.reborn.energy.api.base.SimpleBatteryItem;
 @Environment(EnvType.CLIENT)
 public class GearRebornClient implements ClientModInitializer {
 
+    boolean e = true;
     public static final KeyBinding NV_KEY_BIND = new KeyBinding("key.gearreborn.toggle_nv",
             InputUtil.Type.KEYSYM,
             GLFW.GLFW_KEY_N,
@@ -55,7 +61,20 @@ public class GearRebornClient implements ClientModInitializer {
                 }
         );
 
-    }
+        ClientTickEvents.END_CLIENT_TICK.register(client -> {
+            if (NV_KEY_BIND.isPressed()) {
+                if (!e) return;
+                e = !e;
+                ClientPlayNetworking.send(GearReborn.gogglesTogglePacketIdentifier, PacketByteBufs.empty());
+            }
+            if (!NV_KEY_BIND.isPressed()){
+                e=true;
+            }
+        });
+
+    };
+
+
 
     private static <T extends Item> void registerPredicateProvider(Class<T> itemClass, Identifier identifier, ItemModelPredicateProvider<T> modelPredicateProvider) {
         Registry.ITEM.stream()
